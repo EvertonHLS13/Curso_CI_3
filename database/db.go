4ecf19d9f96efdb2b6f7a500627ca302e64b254b
Everton Holanda Lourenço da Silva <evertonfacul7@gmail.com>
2026-07-14 00:29:15 -0300

    Refactor database Aula 6_ ECS Rollback
    Refactor database connection function to use os.Getenv directly for environment variables.

--- database/db.go
+++ database/db.go
@@ -1,52 +1,25 @@
 package database
 
 import (
-	"fmt"
 	"log"
 	"os"
-
 	"github.com/guilhermeonrails/api-go-gin/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-var DB *gorm.DB
-
-func getEnv(key, defaultValue string) string {
-	value := os.Getenv(key)
-	if value == "" {
-		return defaultValue
-	}
-	return value
-}
+var (
+	DB  *gorm.DB
+	err error
+)
 
 func ConectaComBancoDeDados() {
-
-	host := getEnv("HOST", "localhost")
-	user := getEnv("USER", "postgres")
-	password := getEnv("PASSWORD", "postgres")
-	dbname := getEnv("DBNAME", "postgres")
-	port := getEnv("DBPORT", "5432")
-
-	stringDeConexao := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		host,
-		user,
-		password,
-		dbname,
-		port,
-	)
-
-	db, erro := gorm.Open(postgres.Open(stringDeConexao), &gorm.Config{})
-
-	if erro != nil {
-		log.Println("Erro ao conectar com banco de dados")
-		log.Panic(erro)
+	
+	stringDeConexao := "host="+os.Getenv("HOST")+" user="+os.Getenv("USER")+" password="+os.Getenv("PASSWORD")+" dbname="+os.Getenv("DBNAME")+" port="+os.Getenv("DBPORT")+" sslmode=disable"
+	DB, err = gorm.Open(postgres.Open(stringDeConexao))
+	if err != nil {
+		log.Panic("Erro ao conectar com banco de dados")
 	}
 
-	DB = db
-
-	if erro := DB.AutoMigrate(&models.Aluno{}); erro != nil {
-		log.Println("Erro ao executar AutoMigrate:", erro)
-	}
+	DB.AutoMigrate(&models.Aluno{})
 }
