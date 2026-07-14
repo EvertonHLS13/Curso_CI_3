0dc9962c6f9469bdb3997f9fe2609736cb4b0ebe
Everton Holanda Lourenço da Silva <evertonfacul7@gmail.com>
2026-07-14 00:35:24 -0300

    Refactor database connection and environment variable handling

--- database/db.go
+++ database/db.go
@@ -1,25 +1,49 @@
 package database
 
 import (
+	"fmt"
 	"log"
 	"os"
+
 	"github.com/guilhermeonrails/api-go-gin/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-var (
-	DB  *gorm.DB
-	err error
-)
+var DB *gorm.DB
+
+func getEnv(key, defaultValue string) string {
+	if value, ok := os.LookupEnv(key); ok && value != "" {
+		return value
+	}
+	return defaultValue
+}
 
 func ConectaComBancoDeDados() {
-	
-	stringDeConexao := "host="+os.Getenv("HOST")+" user="+os.Getenv("USER")+" password="+os.Getenv("PASSWORD")+" dbname="+os.Getenv("DBNAME")+" port="+os.Getenv("DBPORT")+" sslmode=disable"
-	DB, err = gorm.Open(postgres.Open(stringDeConexao))
+
+	host := getEnv("DBHOST", "localhost")
+	user := getEnv("DBUSER", "postgres")
+	password := getEnv("DBPASSWORD", "postgres")
+	dbname := getEnv("DBNAME", "postgres")
+	port := getEnv("DBPORT", "5432")
+
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		host,
+		user,
+		password,
+		dbname,
+		port,
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Panic("Erro ao conectar com banco de dados")
+		log.Panic("Erro ao conectar com banco de dados: ", err)
 	}
 
-	DB.AutoMigrate(&models.Aluno{})
+	DB = db
+
+	if err := DB.AutoMigrate(&models.Aluno{}); err != nil {
+		log.Panic("Erro no AutoMigrate: ", err)
+	}
 }
