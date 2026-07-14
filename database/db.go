aa4f440b5fbe97c6acde2b709f06e585e5f2a08a
Everton Holanda Lourenço da Silva <evertonfacul7@gmail.com>
2026-07-14 01:25:44 -0300

    Upgrade DB rollback ECS

--- database/db.go
+++ database/db.go
@@ -27,7 +27,7 @@ func ConectaComBancoDeDados() {
 	host := getEnv("DB_HOST", "localhost")
 	user := getEnv("DB_USER", "postgres")
 	password := getEnv("DB_PASSWORD", "postgres")
-	dbname := getEnv("DB_NAME", "root")
+	dbname := getEnv("DB_NAME", "postgres")
 	port := getEnv("DB_PORT", "5432")
 
 	stringDeConexao := fmt.Sprintf(
@@ -39,18 +39,8 @@ func ConectaComBancoDeDados() {
 		port,
 	)
 
-	log.Println("Conectando no banco:")
-	log.Println(
-		fmt.Sprintf(
-			"host=%s user=%s dbname=%s port=%s",
-			host,
-			user,
-			dbname,
-			port,
-		),
-	)
-
 	db, err := gorm.Open(postgres.Open(stringDeConexao), &gorm.Config{})
+
 	if err != nil {
 		log.Println("Erro ao conectar com banco de dados")
 		log.Panic(err)
@@ -59,7 +49,8 @@ func ConectaComBancoDeDados() {
 	DB = db
 
 	err = DB.AutoMigrate(&models.Aluno{})
+
 	if err != nil {
-		log.Println("Erro ao executar AutoMigrate:", err)
+		log.Println("Erro no AutoMigrate:", err)
 	}
 }
